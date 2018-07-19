ca04719b3012b64ce280d4be7c5bd2c8d898e769
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-07-18 21:36:00 -0400

    Added ability to input name.  Added name to output messages.

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -10,6 +10,8 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
+            Console.Write("Please Enter your Name: ");
+            var name = Console.ReadLine();
             Console.Write("Please enter an integer between 1 and 100:");
             var input = Console.ReadLine();
             if (int.TryParse(input, out var inputAsInteger))
@@ -20,11 +22,11 @@ namespace Lab3
                 }
                 if (inputAsInteger % 2 == 0)
                 {
-                    GenerateEvenOutput(inputAsInteger);
+                    GenerateEvenOutput(name, inputAsInteger);
                 }
                 else
                 {
-                    GenerateOddOutput(inputAsInteger);
+                    GenerateOddOutput(name, inputAsInteger);
                 }
             }
             else
@@ -35,28 +37,28 @@ namespace Lab3
             Console.ReadKey();
         }
 
-        private static void GenerateOddOutput(int inputAsInteger)
+        private static void GenerateOddOutput(string name, int inputAsInteger)
         {
-//Odd Cases
+            //Odd Cases
 
             //There is no need to check input ranges, because they both output the same text.
-            Console.WriteLine($"{inputAsInteger} Odd.");
+            Console.WriteLine($"{name} - {inputAsInteger} Odd.");
         }
 
-        private static void GenerateEvenOutput(int inputAsInteger)
+        private static void GenerateEvenOutput(string name, int inputAsInteger)
         {
-//Even Cases
+            //Even Cases
             if (inputAsInteger >= 2 && inputAsInteger <= 25)
             {
-                Console.WriteLine("Even and less than 25.");
+                Console.WriteLine($"{name} - Even and less than 25.");
             }
             else if (inputAsInteger >= 26 && inputAsInteger <= 60)
             {
-                Console.WriteLine("Even");
+                Console.WriteLine($"{name} - Even");
             }
             else
             {
-                Console.WriteLine($"{inputAsInteger} Even.");
+                Console.WriteLine($"{name} - {inputAsInteger} Even.");
             }
         }
     }
